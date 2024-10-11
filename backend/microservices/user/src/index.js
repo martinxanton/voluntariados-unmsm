da3f95118fad9b3eebac41a5a73eb176da525788
Carlos Mejia <ccmm.05.08@gmail.com>
2024-10-11 17:22:56 -0500

    Add Dockerfile

--- backend/microservices/user/src/index.js
+++ backend/microservices/user/src/index.js
@@ -1,18 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const connectDB = require('./config/db');
-const schema = require('./graphql/shema');
-const resolvers = require('./graphql/resolvers');
+require("dotenv").config();
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
+const schema = require("./graphql/shema");
+const resolvers = require("./graphql/resolvers");
 
 const app = express();
 
 connectDB();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: resolvers,
-  graphiql: true,
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
 
-app.listen(4000, () => console.log('Servidor corriendo en el puerto 4000'));
+app.listen(process.env.PORT, () =>
+  console.log("Servidor corriendo en el puerto 4000")
+);
