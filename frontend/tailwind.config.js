0e68defb90b24cf18451ddfc104d21726b22fb16
Martin Anton <119125486+martinxanton@users.noreply.github.com>
2024-10-12 07:36:28 -0500

    cambiar tema a "fantasy" en index.html y actualizar configuración de daisyui en tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [
     // eslint-disable-next-line no-undef
-    require('daisyui'),
+    require("daisyui"),
   ],
-  daisyui: {
-    themes: ["light", "dark", "cupcake", "dim", "winter"],
+  daisyui: {  
+    themes: [{
+      fantasy: {
+        ...require("daisyui/src/theming/themes")["fantasy"],
+        primary: "#0075bf",
+        secondary: "#6d0076",
+      },
+    },"light", "dark", "cupcake", "dim", "winter", "fantasy", "luxury", "forest", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "black", "bumblebee", "halloween", "garden", "forest", "dracula", "night"],
   },
-}
-
+};
