81e2ca81c395e96e3010b19dc2ec5576db6370bf
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2024-11-18 20:01:23 -0500

    feat(gateway): update structure and fix gateway configuration

---
+++ backend/gateway/src/infrastructure/web/app.module.ts
@@ -0,0 +1,30 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppConfigModule } from '../config/config.module';
+import { AppConfigService } from '../config/config.service';
+
+@Module({
+  imports: [
+    AppConfigModule,
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      imports: [AppConfigModule],
+      inject: [AppConfigService],
+      useFactory: (appConfigService: AppConfigService) => ({
+        // driver: ApolloGatewayDriver,
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              // { name: 'users', url: appConfigService.apiUsersUrl },
+              // { name: 'programs', url: appConfigService.apiProgramsUrl },
+              { name: 'recommender', url: appConfigService.apiRecommenderUrl },
+            ],
+          }),
+        },
+      }),
+      driver: ApolloGatewayDriver,
+    }),
+  ],
+})
+export class AppModule {}
