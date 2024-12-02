3cc7979d76554e0e5a1f6feb60e15b19c918706a
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2024-12-01 20:56:15 -0500

    feat (recommender): implement GraphQL integration for finding volunteering by ID

--- backend/microservices/recommender/src/infrastructure/db/mysql/volunteering.repository.impl.ts
+++ backend/microservices/recommender/src/infrastructure/db/mysql/volunteering.repository.impl.ts
@@ -1,32 +1,55 @@
-// TODO: IMPLEMENTAR CON GRAPHQL
-
+import { Inject } from '@nestjs/common';
 import { Volunteering } from '../../../domain/models/volunteering.model';
 import { IVolunteeringRepository } from '../../../domain/repositories/voluteering.repository.interface';
+import { IConfigService } from '../../../domain/services/config.service.interface';
 
 export class VolunteeringRepository implements IVolunteeringRepository {
-  private volunteerings: Volunteering[] = [
-    new Volunteering(
-      "1",
-      'Enseñanza de niños de primaria',
-      'Educación',
-      'Ayudar a los niños de primaria',
-      'La Victoria',
-      ['Presencial'],
-    ),
-    new Volunteering(
-      "2",
-      'Enseñanza de niños de secundaria',
-      'Educación',
-      'Ayudar a los niños de secundaria',
-      'San Miguel',
-      ['Virtual'],
-    ),
-  ];
+  constructor(
+    @Inject('IConfigService')
+    private readonly configService: IConfigService,
+  ) {}
 
-  constructor() {}
+  async findById(id: string): Promise<Volunteering | null> {
+    const query = `
+      query {
+        getVolunteerById(id: "${id}") {
+          id
+          title
+          category
+          location
+          tags
+        }
+      }
+    `;
 
-  findById(id: string): Promise<Volunteering | null> {
-    const volunteering = this.volunteerings.find((s) => s.id === id);
-    return Promise.resolve(volunteering);
+    try {
+      const response = await fetch(this.configService.volunteeringServiceUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      });
+      const data = await response.json();
+      const result = data['data']['getVolunteerById'];
+      
+      if (!result) {
+        return Promise.resolve(null);
+      }
+
+      // TODO: FIX THIS objective
+
+      const volunteering = new Volunteering(
+        result['id'],
+        result['title'],
+        result['category'],
+        result['objective'] || 'objective',
+        result['location'],
+        result['tags'],
+      );
+      return Promise.resolve(volunteering);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
 }
